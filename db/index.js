036bab6434c42652f744d4a7ae9dffacfee77384
Rushikeshsasamkar <44942941+Rushikeshsasamkar@users.noreply.github.com>
2023-09-19 13:12:36 +0530

    Update index.js

--- db/index.js
+++ db/index.js
@@ -6,7 +6,7 @@ const mongoose = require("mongoose");
 // If no env has been set, we dynamically set it to whatever the folder name was upon the creation of the app
 
 const MONGO_URI =
-  process.env.MONGODB_URI || "mongodb://127.0.0.1:27017/habit-tracker"; // lowercased
+   "mongodb+srv://rushikeshsasamakar:Rushi111@cluster0.9ktk8j6.mongodb.net/"; // lowercased
 
 mongoose
   .connect(MONGO_URI)
